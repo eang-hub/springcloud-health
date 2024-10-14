6ef0398b6e10069e705179eaa92b003f50f6dd76
eang <langye118@gmail.com>
2024-10-14 15:28:26 +0800

    使用 Ribbon 实现客户端负载均衡.html

--- intervention-service/src/main/java/com/springhealth/intervention/InterventionApplication.java
+++ intervention-service/src/main/java/com/springhealth/intervention/InterventionApplication.java
@@ -1,18 +1,18 @@
 package com.springhealth.intervention;
 
-import java.net.UnknownHostException;
-
+import com.springhealth.intervention.config.SpringHealthLoadBalanceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "userservice", configuration = SpringHealthLoadBalanceConfig.class)
 public class InterventionApplication {
 
 	@LoadBalanced
